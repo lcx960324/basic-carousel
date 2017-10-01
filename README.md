279e4023ccb649626a9caa53050aee7ba2712c59
Lawrence Lee <lawrence.lee@foxmail.com>
2017-10-01 23:29:11 +0800

    copy elements

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 00. Move a block smoothly.
 01. Move the block back to the start.
 02. Multi blocks.
+03. Generate blocks automatically.
+04. Copy elements passed in to fill the whole container.
--- carousel.js
+++ carousel.js
@@ -1,11 +1,18 @@
-function carousel (container, image, duration) {
+function carousel (container, image, duration, items) {
+    for (var i = 0; i < items.length; i++) {
+        image.appendChild(items[i].cloneNode(true)) // 防止由于 DOM 已经被添加而不会重复添加
+        if (i === items.length -1 && image.clientWidth < container.clientWidth) i = -1
+    }
+    roll(container, image, duration)
+}
+function roll (container, image, duration) {
     image.style.width = image.scrollWidth + 'px' // 设定宽度防止运动到 container 尾部时因为空间不足导致 div 掉下去。
     image.style.left = '-100%'
     for(let i = -image.clientWidth; i < container.clientWidth; i++) {
         setTimeout(function () {
             image.style.left = i + 'px'
-            if (i === container.clientWidth - 1) carousel(container, image, duration)
+            if (i === container.clientWidth - 1) roll(container, image, duration)
         }, (i + image.clientWidth) * duration)
-        // if (i === container.clientWidth - 1) i = -image.clientWidth
+        // if (i === container.clientWidth - 1) i = -image.clientWidth。
     }
 }
